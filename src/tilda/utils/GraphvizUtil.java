06312c2ed51d08ff3ddb47d55362a65a8006620d
Gadikota Mohan Raja <mohan@devathon.com>
2016-04-29 17:12:10 +0530

    Visualized Object -> Mapper, Object -> Object

--- src/tilda/utils/GraphvizUtil.java
+++ src/tilda/utils/GraphvizUtil.java
@@ -3,18 +3,16 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringJoiner;
-import java.util.regex.Pattern;
-
 import org.apache.logging.log4j.LogManager;
 
-import tilda.parsing.parts.Base;
 import tilda.parsing.parts.Column;
 import tilda.parsing.parts.Enumeration;
+import tilda.parsing.parts.ForeignKey;
 import tilda.parsing.parts.Mapper;
 import tilda.parsing.parts.Object;
 import tilda.parsing.parts.Schema;
@@ -26,12 +24,40 @@ class Hello<T> {
 	protected static final Logger LOG = LogManager.getLogger("Hello");
 
   HashMap<Object, Mapper> mapperObject = new HashMap<Object, Mapper>();
+  HashMap<Object, java.lang.Object> objectMapper = new HashMap<Object, java.lang.Object>();
+
   List<Object> objects;
   List<View> views;
   List<Enumeration> enumerations;
   List<Mapper> mappers;
   String schemaName;
+  private void makeUniq(){
+		Set<Object> hs = new HashSet<Object>();
+		hs.addAll(objects);
+		objects.clear();
+		objects.addAll(hs);
+
+		Set<View> hs1 = new HashSet<View>();
+		hs1.addAll(views);
+		views.clear();
+		views.addAll(hs1);
+
+		
+		Set<Mapper> hs2 = new HashSet<Mapper>();
+		hs2.addAll(mappers);
+		mappers.clear();
+		mappers.addAll(hs2);
+
+		
+		Set<Enumeration> hs3 = new HashSet<Enumeration>();
+		hs3.addAll(enumerations);
+		enumerations.clear();
+		enumerations.addAll(hs3);
+
+  }
   public String toString(){
+	  // To do remove duplicated in mapper|objects|Views|enums
+	  makeUniq();
 	  StringBuilder sb = new StringBuilder();
       HashMap<String, String> hm = new HashMap<String, String>();
       sb.append("digraph Nodes {");
@@ -100,7 +126,7 @@ class Hello<T> {
 	  for(Enumeration obj : enumerations){
 		  String _Name = obj._Name;
 		  if(hm.get(_Name.toLowerCase()) == null){
-			  sb.append(""+_Name+"[label=\"{"+_Name+"}|\"]");
+			  sb.append(""+_Name+"[label=\"{"+_Name+"}\"]");
 			  sb.append("\r\n");
 			  hm.put(_Name.toLowerCase(), "");
 		  }
@@ -132,7 +158,7 @@ class Hello<T> {
 		  sb.append(" }");
 		  sb.append("\r\n");
 	  }
-
+      // Mapper -> object
 	  if(!mapperObject.isEmpty()){
           Iterator it = mapperObject.entrySet().iterator();
 		  while(it.hasNext()){
@@ -146,6 +172,17 @@ class Hello<T> {
 		      }
 		  }
 	  }
+	  if(!objectMapper.isEmpty()){
+          Iterator it = objectMapper.entrySet().iterator();
+		  while(it.hasNext()){
+		      Map.Entry pair = (Map.Entry)it.next();
+		      Object obj1 = (Object)pair.getKey();
+		      java.lang.Object obj = pair.getValue();
+		      sb.append((obj instanceof Mapper) ? ((Mapper)obj)._Name+" -> "+obj1._Name : ((Object) obj)._Name+" -> "+obj1._Name);
+			  sb.append("\r\n");
+		  }
+	  }
+	  // Object -> Object , Object -> Mapper
       sb.append("}");
 	  return sb.toString();
   }
@@ -159,8 +196,57 @@ class Hello<T> {
 //  }
 
   
-  private void mapObjectRelation(Object obj){
-	  
+  public void mapObjectRelation(){
+	  List<Object> tCollection = new ArrayList<Object>();
+	  for(Object obj : objects){
+		  for(Column c : obj._Columns){
+			  if(c._Mapper != null){
+			        try{
+			        	Mapper map = null;
+			        	for(Mapper mapper : mappers){
+			        		if(mapper._Name.equalsIgnoreCase(c._Mapper._DestObject)){
+			        			map = mapper;
+			        			break;
+			        		}
+			        	}
+			        	if(map == null){
+			        		map = new Mapper();
+			        		map._Name = c._Mapper._DestObject;
+			        	}
+			        	objectMapper.put(obj, map);
+			        } catch(Exception e){
+			          continue;
+			        }
+			  }
+		  }
+		  for(ForeignKey fk : obj._ForeignKeys){
+			  Object obj2 = null;
+			  for(Object currentSchemaObject : objects){
+				  if(currentSchemaObject._Name.equalsIgnoreCase(fk._DestObjectObj._Name)){
+					  obj2 = currentSchemaObject;
+					  break;
+				  }
+			  }
+			  if(obj2 == null){
+				  for(Object otherSchemaObject : tCollection){
+					  if(otherSchemaObject._Name.equalsIgnoreCase(fk._DestObjectObj._Name)){
+						  obj2 = otherSchemaObject;
+						  break;
+					  }
+				  }
+			  }
+			  if(obj2 == null){
+				  obj2 = new Object();
+				  obj2._Name = fk._DestObjectObj._Name;
+				  tCollection.add(obj2);
+			  }
+			  LOG.error("Key --> "+obj2._Name);
+			  LOG.error("Object Key --> "+obj._Name);
+
+			  objectMapper.put(obj, obj2);
+		  }
+	  }
+	  objects.addAll(tCollection);
   }
   
   private void mapViewRelation(View obj){
@@ -168,7 +254,7 @@ class Hello<T> {
   }
   
   public void mapMapperRelation(){
-	  List<Object> tCollection = new ArrayList<Object>();
+	  Set<Object> tCollection = new HashSet<Object>();
 	  for(Mapper obj : mappers){
 		  for(Column c : obj._PrimaryColumns){
 			  String[] _SameAs = c._SameAs.split("\\.");
@@ -176,7 +262,7 @@ class Hello<T> {
 				  continue;
 			  }
 			  String key = _SameAs[_SameAs.length - 2];
-
+			  LOG.error("Key ---> "+key);
 			  for(Object object : objects){
 				  if(key.equalsIgnoreCase(object._Name)){
 					  mapperObject.put(object, obj);
@@ -212,6 +298,7 @@ public class GraphvizUtil{
       hello.views = schema._Views;
       hello.enumerations = schema._Enumerations;
       hello.mapMapperRelation();
+      hello.mapObjectRelation();
       hello.schemaName = schema._Name;
       LOG.error("DOne");
       LOG.error(hello);
