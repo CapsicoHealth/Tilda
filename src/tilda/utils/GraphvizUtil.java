cfb39df033b2e9bb3064f802178226173d0917a9
Gadikota Mohan Raja <mohan@devathon.com>
2016-05-02 20:35:43 +0530

    Dot file generation done from Tilda JSON schema.

--- src/tilda/utils/GraphvizUtil.java
+++ src/tilda/utils/GraphvizUtil.java
@@ -1,15 +1,21 @@
 package tilda.utils;
 import java.io.File;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.apache.logging.log4j.LogManager;
 
+import tilda.enums.FrameworkSourcedType;
 import tilda.parsing.parts.Column;
 import tilda.parsing.parts.Enumeration;
 import tilda.parsing.parts.ForeignKey;
@@ -17,49 +23,181 @@ import tilda.parsing.parts.Mapper;
 import tilda.parsing.parts.Object;
 import tilda.parsing.parts.Schema;
 import tilda.parsing.parts.View;
+import tilda.parsing.parts.ViewColumn;
 
 import org.apache.logging.log4j.Logger;
 
+import com.sun.media.sound.FFT;
+
 class Hello<T> {
-	protected static final Logger LOG = LogManager.getLogger("Hello");
+  public static <T> ArrayList<List<T>> chunks(ArrayList<T> bigList,int n){
+	  ArrayList<List<T>> chunks = new ArrayList<List<T>>();
+	  for (int i = 0; i < bigList.size(); i += n) {
+		  int start = i;
+	      int end = Math.min(bigList.size(), i + n);
+	      chunks.add(bigList.subList(start, end));
+	  }
+	return chunks;
+  }
+  protected static final Logger LOG = LogManager.getLogger("Hello");
 
   HashMap<Object, Mapper> mapperObject = new HashMap<Object, Mapper>();
   HashMap<Object, java.lang.Object> objectMapper = new HashMap<Object, java.lang.Object>();
+
+  HashMap<Object, View> viewMapper = new HashMap<Object, View>();
 
   List<Object> objects;
   List<View> views;
   List<Enumeration> enumerations;
   List<Mapper> mappers;
   String schemaName;
-  private void makeUniq(){
-		Set<Object> hs = new HashSet<Object>();
-		hs.addAll(objects);
-		objects.clear();
-		objects.addAll(hs);
-
-		Set<View> hs1 = new HashSet<View>();
-		hs1.addAll(views);
-		views.clear();
-		views.addAll(hs1);
-
-		
-		Set<Mapper> hs2 = new HashSet<Mapper>();
-		hs2.addAll(mappers);
-		mappers.clear();
-		mappers.addAll(hs2);
-
-		
-		Set<Enumeration> hs3 = new HashSet<Enumeration>();
-		hs3.addAll(enumerations);
-		enumerations.clear();
-		enumerations.addAll(hs3);
+  Schema schema;
+  public Hello(Schema schema){
+	  this.schema = schema;
+      this.objects = schema._Objects;
+      this.mappers = schema._Mappers;
+      this.views = schema._Views;
+      this.enumerations = schema._Enumerations;
+      this.schemaName = schema._Name;
 
   }
+  private void makeUniq(){
+	  removeDuplicates(objects);
+	  removeDuplicates(views);
+	  removeDuplicates(mappers);
+	  removeDuplicates(enumerations);
+  }
+  
+  private LinkedHashSet<String> getViewObjects(View view){
+     LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+	 for(ViewColumn viewColumn : view._ViewColumns){
+		  Object viewColumnObject = viewColumn._SameAsObj._ParentObject; 
+		  if(viewColumnObject._FST == FrameworkSourcedType.NONE)
+			  lhs.add(viewColumnObject._Name.toUpperCase());
+     }
+	 return lhs;
+  }
+  
+  private LinkedHashSet<String> getMapperObjectNames(Mapper mapper){
+     LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+	  for(Column c : mapper._PrimaryColumns){
+		  if(c._SameAsObj == null || c._SameAsObj._ParentObject == null){
+			  continue;
+		  }
+		  Object obj = c._SameAsObj._ParentObject;
+		  if(obj._FST == FrameworkSourcedType.NONE){
+			  String key = obj._Name.toUpperCase();
+			  lhs.add(key);
+		  }
+	  }
+
+	 return lhs;
+  }
+  
+  private LinkedHashSet<String> getObjectEnums(Object object){
+	  LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+	  for(Column c : object._Columns){
+		  if(c._Enum == null){
+			  continue;
+		  }
+		  Object obj = c._Enum._DestObjectObj;
+		  if(obj._FST == FrameworkSourcedType.ENUMERATION){
+			  String key = obj._Name.toUpperCase();
+			  lhs.add(key);
+		  }
+	  }
+	  return lhs;
+
+  }
+  
+  private LinkedHashSet<String> getObjectMappers(Object object){
+	  LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+	  for(Column c : object._Columns){
+		  if(c._SameAsObj == null || c._SameAsObj._Mapper == null || c._SameAsObj._ParentObject == null){
+			  continue;
+		  }
+		  Object obj = c._SameAsObj._ParentObject;
+		  if(obj._FST == FrameworkSourcedType.MAPPER){
+			  String key = obj._Name.toUpperCase();
+			  lhs.add(key);
+		  }
+	  }
+	  return lhs;
+  }
+  
+  private <T> boolean containsObject(List<T> collection, java.lang.Object object){
+	  boolean flag = false;
+	  String name = null;
+	  if(object instanceof Object){
+		  name = ((Object)object)._Name;
+	  }
+	  
+	  else if(object instanceof View){
+		  name = ((View)object)._Name;
+	  }
+	  
+	  
+	  else if(object instanceof Enumeration){
+		  name = ((Enumeration)object)._Name;
+	  }
+	  
+	  
+	  else if(object instanceof Mapper){
+		  name = ((Mapper)object)._Name;
+	  }
+
+	    for(T obj : collection){
+	        Field field;
+			try {
+				field = obj.getClass().getField("_Name");
+			    String objName = (String)field.get(obj);
+			    flag = objName.equalsIgnoreCase(name);
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				LOG.error(e.getMessage());
+				e.printStackTrace();
+			}
+		   if(flag)
+			   break;
+
+	    }
+
+	  
+	  return flag;
+  }
+  
+  private <T> void removeDuplicates(List<T> list){
+		List<T> sortedSet = new ArrayList<T>();
+		sortedSet.addAll(list);
+
+	    list.clear();
+	    for(T obj : sortedSet){
+	        Field field;
+	        boolean hasDup = false;
+			try {
+			   field = obj.getClass().getField("_Name");
+			   for(T obj2 : list ){
+				    String objName = (String)field.get(obj);
+				    String obj2Name = (String)field.get(obj2);
+				    hasDup = objName.equalsIgnoreCase(obj2Name);
+				    if(hasDup)
+				    	break;
+			   }
+			   if(!hasDup){
+				   list.add(obj);
+			   }
+	
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+		sortedSet.clear();
+  }
+
   public String toString(){
-	  // To do remove duplicated in mapper|objects|Views|enums
-	  makeUniq();
+	  // makeUniq();
 	  StringBuilder sb = new StringBuilder();
-      HashMap<String, String> hm = new HashMap<String, String>();
       sb.append("digraph Nodes {");
       sb.append("\r\n");
       sb.append("fontname = \"Bitstream Vera Sans\"");
@@ -84,206 +222,145 @@ class Hello<T> {
       sb.append("\r\n");
       sb.append("]");
       sb.append("\r\n");
-      List<String> arr = new ArrayList<String>();
-	  for(View obj : views){
-		  String _Name = obj._Name;
-		  if(hm.get(_Name.toLowerCase()) == null){
-			sb.append(""+_Name+"[shape=parallelogram, label=\""+_Name+"\"]");
-			sb.append("\r\n");
-			arr.add(_Name);
-			hm.put(_Name.toLowerCase(), "");
-		  }
-	  }
-	  if(!arr.isEmpty()){
-		  sb.append("{rank=same");
-		  for(String s : arr){
-			  sb.append("; "+s);
-		  }
-		  sb.append(" }");
-		  sb.append("\r\n");
-	  }
-	  arr.clear();
-	  for(Mapper obj : mappers){
-		  String _Name = obj._Name;
-		  if(hm.get(_Name.toLowerCase()) == null){
-			sb.append(""+_Name+"[label=\"{"+_Name+"}\"]");
-			sb.append("\r\n");
-			arr.add(_Name);
-			hm.put(_Name.toLowerCase(), "");
-		  }
-	  }
-	  if(!arr.isEmpty()){
-		  sb.append("{rank=same");
-		  for(String s : arr){
-			  sb.append("; "+s);
-		  }
-		  sb.append(" }");
-		  sb.append("\r\n");
+      TreeSet<String> arr = new TreeSet<String>();
 
-	  }
-
-	  arr.clear();
-	  for(Enumeration obj : enumerations){
-		  String _Name = obj._Name;
-		  if(hm.get(_Name.toLowerCase()) == null){
-			  sb.append(""+_Name+"[label=\"{"+_Name+"}\"]");
+	  for(Object obj : objects){
+		  String _Name = obj._Name.toUpperCase();
+		  if(obj._FST ==  FrameworkSourcedType.NONE){
+			  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"red\"]");
 			  sb.append("\r\n");
-			  hm.put(_Name.toLowerCase(), "");
+		  }
+		  else if(obj._FST ==  FrameworkSourcedType.ENUMERATION){
+			  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"cyan\"]");
+			  sb.append("\r\n");
+		  }
+		  else if(obj._FST == FrameworkSourcedType.MAPPER){
+			  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"blue\"]");
+			  sb.append("\r\n");
+		  } else if(obj._FST == FrameworkSourcedType.VIEW){
+				sb.append(""+_Name+"[shape=parallelogram, label=\""+_Name+"\", color=\"green\"]");
+				sb.append("\r\n");		  
 		  }
 	  }
-	  if(!arr.isEmpty()){
-		  sb.append("{rank=same;");
-		  for(String s : arr){
-			  sb.append("; "+s);
+	  // Mapper --> Object
+	  for(Mapper mapper : mappers){
+		  LinkedHashSet<String> listS = getMapperObjectNames(mapper);
+		  for(String s : listS){
+			  Object tObject = new Object();
+			  tObject._Name = s.toUpperCase();
+			  if(!containsObject(objects, tObject)){
+				  objects.add(tObject);
+				  String _Name = tObject._Name.toUpperCase();
+				  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"red\"]");
+				  sb.append("\r\n");
+			  }
+		      sb.append(mapper._Name.toUpperCase()+" -> "+s.toUpperCase()+"[color=\"blue\"]");
+			  sb.append("\r\n");
 		  }
-		  sb.append(" }");
-		  sb.append("\r\n");
+	  }
+	  // Object -> Object	  
+	  for(Object object : objects){
+		  for(ForeignKey fKey : object._ForeignKeys){
+			  String fKeyS = fKey._DestObjectObj._Name;
+			  if(fKeyS.equalsIgnoreCase("zoneinfo"))
+				  continue;
+			  Object tObject = new Object();
+			  tObject._Name = fKeyS.toUpperCase();
+			  if(!containsObject(objects, tObject)){
+				  // objects.add(tObject); TODO add object to objects collection
+				  String _Name = tObject._Name.toUpperCase();
+				  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"red\"]");
+				  sb.append("\r\n");
+			  }
+			  sb.append(object._Name.toUpperCase()+" -> "+fKeyS.toUpperCase()+"[color=\"red\"]");
+			  sb.append("\r\n");
+		  }
+	  }
+	  // Object --> Mapper
+	  // Object to Enum
+
+	  for(Object object : objects){
+		  if(object._FST != FrameworkSourcedType.NONE){
+			  continue;
+		  }
+		  LinkedHashSet<String> listS = getObjectMappers(object);
+		  for(String s : listS){
+			  Mapper tMapper = new Mapper();
+			  tMapper._Name = s.toUpperCase();
+			  if(!containsObject(objects, tMapper)){
+				  mappers.add(tMapper);
+				  String _Name = tMapper._Name.toUpperCase();
+				  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"blue\"]");
+				  sb.append("\r\n");
+			  }
+			  sb.append(object._Name.toUpperCase()+" -> "+s.toUpperCase());
+			  sb.append("\r\n");
+		  }
+		  listS.clear();
+		  listS = getObjectEnums(object);
+		  for(String s : listS){
+			  Enumeration tEnum = new Enumeration();
+			  tEnum._Name = s.toUpperCase();
+			  if(!containsObject(objects, tEnum)){
+				  enumerations.add(tEnum);
+				  String _Name = tEnum._Name.toUpperCase();
+				  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"cyan\"]");
+				  sb.append("\r\n");
+			  }
+			  sb.append(object._Name.toUpperCase()+" -> "+s.toUpperCase()+"[color=\"cyan\"]");
+			  sb.append("\r\n");
+		  }
 	  }
 	  
-	  arr.clear();
-	  for(Object obj : objects){
-		  String _Name = obj._Name;
-		  if(hm.get(_Name.toLowerCase()) == null){
-			  sb.append(""+_Name+"[label=\"{"+_Name+"}\"]");
-			  sb.append("\r\n");
-			  arr.add(_Name);
-			  hm.put(_Name.toLowerCase(), "");
-		  }
-	  }
-	  if(!arr.isEmpty()){
-		  sb.append("{rank=same");
-		  for(String s : arr){
-			  sb.append("; "+s);
-		  }
-		  sb.append(" }");
-		  sb.append("\r\n");
-	  }
-      // Mapper -> object
-	  if(!mapperObject.isEmpty()){
-          Iterator it = mapperObject.entrySet().iterator();
-		  while(it.hasNext()){
-		      Map.Entry pair = (Map.Entry)it.next();
-		      Object obj1 = (Object)pair.getKey();
-		      Mapper obj2 = (Mapper)pair.getValue();
-		      if(hm.get(obj1._Name.toLowerCase()+"mapper") == null){
-			      sb.append(obj2._Name+" -> "+obj1._Name);
+	  // View -> Object
+	  for(View view : views){
+		  LinkedHashSet<String> listS = getViewObjects(view);
+		  for(String s : listS){
+			  Object tObject = new Object();
+			  tObject._Name = s.toUpperCase();
+			  if(!containsObject(objects, tObject)){
+				  objects.add(tObject);
+				  String _Name = tObject._Name.toUpperCase();
+				  sb.append(""+_Name+"[label=\"{"+_Name+"}\", color=\"red\"]");
 				  sb.append("\r\n");
-				  hm.put(obj1._Name.toLowerCase()+"mapper", "");
-		      }
-		  }
-	  }
-	  if(!objectMapper.isEmpty()){
-          Iterator it = objectMapper.entrySet().iterator();
-		  while(it.hasNext()){
-		      Map.Entry pair = (Map.Entry)it.next();
-		      Object obj1 = (Object)pair.getKey();
-		      java.lang.Object obj = pair.getValue();
-		      sb.append((obj instanceof Mapper) ? ((Mapper)obj)._Name+" -> "+obj1._Name : ((Object) obj)._Name+" -> "+obj1._Name);
+			  }
+			  sb.append(view._Name+" -> "+s.toUpperCase()+"[color=\"green\"]");
 			  sb.append("\r\n");
 		  }
 	  }
-	  // Object -> Object , Object -> Mapper
+
+
       sb.append("}");
 	  return sb.toString();
   }
-//  static Pattern getPattern(){
-//	  StringJoiner strJ = new StringJoiner("|");
-//	  Set<String> keys = mapper.keySet();
-//	  for(String s: keys){
-//		  strJ.add(s);
+  
+//  public void mapMapperRelation(){
+//	  Set<Object> tCollection = new HashSet<Object>();
+//	  for(Mapper mapper : mappers){
+//		  for(String s : getMapperObjectNames(mapper)){
+//			  for(Object object : objects){
+//				  Object tObject = null;
+//				  if(object._Name.equalsIgnoreCase(s)){
+//					  tObject = object;
+//					  break;
+//				  } else {
+//					  for(Object t : tCollection){
+//						  if(t._Name.equalsIgnoreCase(object._Name)){
+//							  tObject = t;
+//							  break;
+//						  }
+//					  }
+//					  if(tObject == null){
+//						  tObject = new Object();
+//						  tObject._Name = object._Name;
+//					  }
+//				  }
+//				  mapperObject.put(tObject, mapper);
+//			  }
+//		  }
 //	  }
-//	  return Pattern.compile(strJ.toString());
+//	  objects.addAll(tCollection);
 //  }
-
-  
-  public void mapObjectRelation(){
-	  List<Object> tCollection = new ArrayList<Object>();
-	  for(Object obj : objects){
-		  for(Column c : obj._Columns){
-			  if(c._Mapper != null){
-			        try{
-			        	Mapper map = null;
-			        	for(Mapper mapper : mappers){
-			        		if(mapper._Name.equalsIgnoreCase(c._Mapper._DestObject)){
-			        			map = mapper;
-			        			break;
-			        		}
-			        	}
-			        	if(map == null){
-			        		map = new Mapper();
-			        		map._Name = c._Mapper._DestObject;
-			        	}
-			        	objectMapper.put(obj, map);
-			        } catch(Exception e){
-			          continue;
-			        }
-			  }
-		  }
-		  for(ForeignKey fk : obj._ForeignKeys){
-			  Object obj2 = null;
-			  for(Object currentSchemaObject : objects){
-				  if(currentSchemaObject._Name.equalsIgnoreCase(fk._DestObjectObj._Name)){
-					  obj2 = currentSchemaObject;
-					  break;
-				  }
-			  }
-			  if(obj2 == null){
-				  for(Object otherSchemaObject : tCollection){
-					  if(otherSchemaObject._Name.equalsIgnoreCase(fk._DestObjectObj._Name)){
-						  obj2 = otherSchemaObject;
-						  break;
-					  }
-				  }
-			  }
-			  if(obj2 == null){
-				  obj2 = new Object();
-				  obj2._Name = fk._DestObjectObj._Name;
-				  tCollection.add(obj2);
-			  }
-			  LOG.error("Key --> "+obj2._Name);
-			  LOG.error("Object Key --> "+obj._Name);
-
-			  objectMapper.put(obj, obj2);
-		  }
-	  }
-	  objects.addAll(tCollection);
-  }
-  
-  private void mapViewRelation(View obj){
-	  
-  }
-  
-  public void mapMapperRelation(){
-	  Set<Object> tCollection = new HashSet<Object>();
-	  for(Mapper obj : mappers){
-		  for(Column c : obj._PrimaryColumns){
-			  String[] _SameAs = c._SameAs.split("\\.");
-			  if(_SameAs.length < 2){
-				  continue;
-			  }
-			  String key = _SameAs[_SameAs.length - 2];
-			  LOG.error("Key ---> "+key);
-			  for(Object object : objects){
-				  if(key.equalsIgnoreCase(object._Name)){
-					  mapperObject.put(object, obj);
-				  }
-				  else{
-					  Object t1 = new Object();
-					  t1._Name = key;
-					  tCollection.add(t1);
-					  mapperObject.put(t1, obj);
-				  }
-			  }
-
-		  }
-	  }
-	  objects.addAll(tCollection);
-  }
-  
-  private void mapEnumerationRelation(Enumeration obj){
-	  
-  }
-
 }
 public class GraphvizUtil{
   protected static final Logger LOG = LogManager.getLogger(GraphvizUtil.class.getName());
@@ -291,17 +368,7 @@ public class GraphvizUtil{
   public static void docs(Schema schema) {
       File output = new File("output_"+schema._Name+".dot");
       PrintWriter printer = null;
-//      String nodeS = null;
-	  Hello hello = new Hello();
-      hello.objects = schema._Objects;
-      hello.mappers = schema._Mappers;
-      hello.views = schema._Views;
-      hello.enumerations = schema._Enumerations;
-      hello.mapMapperRelation();
-      hello.mapObjectRelation();
-      hello.schemaName = schema._Name;
-      LOG.error("DOne");
-      LOG.error(hello);
+	  Hello hello = new Hello(schema);
       try{
     	  printer = new PrintWriter(output);
     	  printer.write(hello.toString());
