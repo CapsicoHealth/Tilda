f0ebd6da72d9727d0e734257af16e6190ab2ff56
Gadikota Mohan Raja <mohan@devathon.com>
2016-05-16 23:40:24 +0530

    Have seperate package for graphviz generator

--- src/tilda/generation/graphviz/GraphvizUtil.java
+++ src/tilda/generation/graphviz/GraphvizUtil.java
@@ -1,4 +1,4 @@
-package tilda.generation;
+package tilda.generation.graphviz;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
---
+++ src/tilda/generation/graphviz/HTMLGenUtil.java
@@ -0,0 +1,5 @@
+package tilda.generation.graphviz;
+
+public class HTMLGenUtil {
+
+}
