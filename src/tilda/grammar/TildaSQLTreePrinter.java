5b7d940788b376883646d2599dd1703f48ee9208
Laurent Hasson <ldh@laurent-hasson.com>
2016-05-26 10:28:26 -0400

    - more work

--- src/tilda/grammar/TildaSQLTreePrinter.java
+++ src/tilda/grammar/TildaSQLTreePrinter.java
@@ -39,20 +39,21 @@ import tilda.utils.SystemValues;
 
 public class TildaSQLTreePrinter extends TildaSQLBaseListener
   {
-    protected static final Logger  LOG   = LogManager.getLogger(TildaSQLTreePrinter.class.getName());
+    protected static final Logger LOG     = LogManager.getLogger(TildaSQLTreePrinter.class.getName());
 
-    protected List<String> _Errors = new ArrayList<String>();
-    
+    protected List<String>        _Errors = new ArrayList<String>();
+
     protected void addError(String err, ParserRuleContext ctx)
-     {
-       err += " (line "+ctx.getStart().getLine()+":"+ctx.getStart().getCharPositionInLine()+")";
-       _Errors.add(err);
-       LOG.error(err);
-     }
-    
-    private final List<String>     _RuleNames;
-    private final StringBuilder    _Str  = new StringBuilder();
-    Map<RuleContext, List<String>> _Path = new HashMap<RuleContext, List<String>>();
+      {
+        err += " (line " + ctx.getStart().getLine() + ":" + ctx.getStart().getCharPositionInLine() + ")";
+        _Errors.add(err);
+        LOG.error(err);
+      }
+
+    protected StringBuilder                  _CodeGen      = new StringBuilder("Q");
+    protected StringBuilder                  _ParseTreeStr = new StringBuilder();
+    protected List<String>                   _RuleNames;
+    protected Map<RuleContext, List<String>> _Path         = new HashMap<RuleContext, List<String>>();
 
     public TildaSQLTreePrinter(Parser parser)
       {
@@ -71,19 +72,55 @@ public class TildaSQLTreePrinter extends TildaSQLBaseListener
         _Path.get(node.getParent()).add(node.getText());
       }
 
-    @Override 
+    @Override
+    public void enterSub_expr(tilda.grammar.TildaSQLParser.Sub_exprContext ctx)
+      {
+        _CodeGen.append(".openPar()");
+      };
+
+    @Override
+    public void exitSub_expr(tilda.grammar.TildaSQLParser.Sub_exprContext ctx)
+      {
+        _CodeGen.append(".closePar()");
+      };
+
+    @Override
+    public void enterBin_operator(tilda.grammar.TildaSQLParser.Bin_operatorContext ctx)
+      {
+        String val = ctx.getText().toLowerCase();
+        if (val.equals("and") == true)
+         _CodeGen.append(".and()");
+        else if (val.equals("or") == true)
+         _CodeGen.append(".or()");
+        else if (val.equals("=") == true || val.equals("==") == true)
+          {
+            _CodeGen.append(".equals(");
+            LOG.debug("Node: "+ctx.toString());
+            LOG.debug("    CHILDREN -> ");
+            for (int i = 0; i < ctx.children.size(); ++i)
+              LOG.debug("      "+i+": "+ctx.children.get(i).getText());
+            RuleContext parent = ctx.parent;
+            LOG.debug("    PARENT -> "+parent.toString());
+            LOG.debug("         SIBLINGS -> ");
+            for (int i = 0; i < parent.getChildCount(); ++i)
+              LOG.debug("          "+i+": "+parent.getChild(i).getText());
+            _CodeGen.append(")");
+          }
+      };
+
+
+    @Override
     public void enterTimestamp_literal(tilda.grammar.TildaSQLParser.Timestamp_literalContext ctx)
-     {
-       String val = ctx.getText();
-       val = val.substring(1, val.length()-1);
-       ZonedDateTime ZDT = DateTimeUtil.parsefromJSON(val);
-       if (ZDT == null)
-        addError("Invalid timestamp "+val, ctx);
-       else
-        LOG.debug("Timestamp parsed: "+val+" -> "+DateTimeUtil.printDateTimeForJSON(ZDT));
-     }
-
-
+      {
+        String val = ctx.getText();
+        val = val.substring(1, val.length() - 1);
+        ZonedDateTime ZDT = DateTimeUtil.parsefromJSON(val);
+        if (ZDT == null)
+          addError("Invalid timestamp " + val, ctx);
+        else
+          LOG.debug("Timestamp parsed: " + val + " -> " + DateTimeUtil.printDateTimeForJSON(ZDT));
+      }
+    
     @Override
     public void enterEveryRule(ParserRuleContext ctx)
       {
@@ -94,7 +131,7 @@ public class TildaSQLTreePrinter extends TildaSQLBaseListener
 
         int ruleIndex = ctx.getRuleIndex();
         _Path.get(ctx).add(ruleIndex >= 0 && ruleIndex < _RuleNames.size() ? _RuleNames.get(ruleIndex)
-        : Integer.toString(ruleIndex));
+                                                                           : Integer.toString(ruleIndex));
       }
 
     @Override
@@ -104,25 +141,18 @@ public class TildaSQLTreePrinter extends TildaSQLBaseListener
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < ruleStack.size(); ++i)
           {
-            sb.append(PaddingUtil.getPad(_Path.size()*3)).append(ruleStack.get(i)).append(SystemValues.NEWLINE);
+            sb.append(PaddingUtil.getPad(_Path.size() * 3)).append(ruleStack.get(i)).append(SystemValues.NEWLINE);
           }
         _Path.get(ctx.parent).add(sb.toString());
 
         if (ctx.parent == null)
-          _Str.append(sb.toString());
+          _ParseTreeStr.append(sb.toString());
       }
-
-    @Override
-    public String toString()
-      {
-        return _Str.toString();
-      }
-
 
     public static String print(ParseTree parseTree, final Parser parser)
       {
         TildaSQLTreePrinter l = new TildaSQLTreePrinter(parser);
         ParseTreeWalker.DEFAULT.walk(l, parseTree);
-        return l.toString();
+        return l._ParseTreeStr.toString()+SystemValues.NEWLINEx2+l._CodeGen.toString();
       }
   }
