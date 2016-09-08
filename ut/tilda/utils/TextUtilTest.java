f1f9fe39611bef2d49873282d2a287da514b58fa
Laurent Hasson <ldh@laurent-hasson.com>
2016-09-08 18:02:43 -0400

    - Was challenged today about why we hand-coded TextUtil.SimplifyName() the way we did. Now they know.

--- ut/tilda/utils/TextUtilTest.java
+++ ut/tilda/utils/TextUtilTest.java
@@ -19,6 +19,8 @@ package tilda.utils;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -29,11 +31,12 @@ public class TextUtilTest
 
     public static void main(String[] args)
       {
-        Test1();
-        Test2();
-        Test3();
-        Test4();
-        Test5();
+        Test_SimplifyName();
+//        Test1();
+//        Test2();
+//        Test3();
+//        Test4();
+//        Test5();
       }
 
     private static void Test5()
@@ -180,4 +183,161 @@ public class TextUtilTest
           }
         System.out.println("String.replaceAll: " + DurationUtil.PrintDuration(System.nanoTime() - T0));
       }
+    
+    
+    
+    
+    
+    public static String SimplifyName_Custom1(String Name)
+      {
+        if (Name == null)
+          return null;
+        StringBuilder Str = new StringBuilder(Name.length());
+        char C[] = Name.toCharArray();
+        for (char c : C)
+          {
+            if (Character.isLetterOrDigit(c) == false)
+              continue;
+            Str.append(Character.toLowerCase(c));
+          }
+        return Str.toString();
+      }
+    public static String SimplifyName_Custom2(String Name)
+      {
+        if (Name == null)
+          return null;
+        StringBuilder Str = new StringBuilder(Name.length());
+        for (int i = 0; i < Name.length(); ++i)
+          {
+            char c = Name.charAt(i);
+            if (Character.isLetterOrDigit(c) == true)
+             Str.append(Character.toLowerCase(c));
+          }
+        return Str.toString();
+      }
+    public static String SimplifyName_Regex1(String Name)
+      {
+        if (Name == null)
+          return null;
+        return Name.replaceAll("[^A-Za-z0-9]+", "").toLowerCase();
+      }
+    protected static Pattern P = Pattern.compile("([^A-Za-z0-9]+)");
+    public static String SimplifyName_Regex2(String Name)
+      {
+        if (Name == null)
+          return null;
+        Matcher M = P.matcher(Name);
+        StringBuilder Str = new StringBuilder();
+        int pos = 0;
+        while (M.find() == true)
+          {
+            if (pos != M.start())
+             Str.append(Name.substring(pos, M.start()));
+            pos=M.end();
+          }
+        if (pos < Name.length())
+         Str.append(Name.substring(pos, Name.length()));
+
+        return Str.toString().toLowerCase();
+      }
+    public static String SimplifyName_Regex3(String Name)
+      {
+        if (Name == null)
+          return null;
+        Matcher M = P.matcher(Name);
+        StringBuffer Str = new StringBuffer();
+        while (M.find() == true)
+          M.appendReplacement(Str, "");
+        M.appendTail(Str);
+        return Str.toString().toLowerCase();
+      }
+    public static String SimplifyName_Regex4(String Name)
+      {
+        if (Name == null)
+          return null;
+        return P.matcher(Name).replaceAll("").toLowerCase();
+      }
+    
+    private static void Test_SimplifyName()
+     {
+       String test = "      This is a 475 a long *&*   test, and with #$^# lots of @#%@weird chars.   ";
+       
+       String VCustom1 = SimplifyName_Custom1(test);
+       String VCustom2 = SimplifyName_Custom2(test);
+       String VRegex1  = SimplifyName_Regex1 (test);
+       String VRegex2  = SimplifyName_Regex2 (test);
+       String VRegex3  = SimplifyName_Regex3 (test);
+       String VRegex4  = SimplifyName_Regex4 (test);
+       
+       if (VCustom1.equals(VCustom2) == false)
+         {
+           LOG.error("ERROR: VCustom1 = '"+VCustom1+"'.");
+           LOG.error("ERROR: VCustom2 = '"+VCustom2+"'.");
+         }
+
+       if (VCustom1.equals(VRegex1) == false)
+         {
+           LOG.error("ERROR: VCustom1 = '"+VCustom1+"'.");
+           LOG.error("ERROR: VRegex1  = '"+VRegex1+"'.");
+         }
+       if (VCustom1.equals(VRegex2) == false)
+         {
+           LOG.error("ERROR: VCustom1 = '"+VCustom1+"'.");
+           LOG.error("ERROR: VRegex1  = '"+VRegex2+"'.");
+         }
+       if (VCustom1.equals(VRegex3) == false)
+         {
+           LOG.error("ERROR: VCustom1 = '"+VCustom1+"'.");
+           LOG.error("ERROR: VRegex3  = '"+VRegex3+"'.");
+         }
+       if (VCustom1.equals(VRegex4) == false)
+         {
+           LOG.error("ERROR: VCustom1 = '"+VCustom1+"'.");
+           LOG.error("ERROR: VRegex4  = '"+VRegex4+"'.");
+         }
+       
+       final int count = 100000;
+       
+       long T0 = System.nanoTime();
+       for (int i = 0; i < count; ++i)
+         {
+           SimplifyName_Custom1(test);
+         }
+       LOG.info("SimplifyName_Custom1: " + DurationUtil.PrintPerformancePerSecond(System.nanoTime() - T0, count)+" ops.");
+
+       T0 = System.nanoTime();
+       for (int i = 0; i < count; ++i)
+         {
+           SimplifyName_Custom2(test);
+         }
+       LOG.info("SimplifyName_Custom2: " + DurationUtil.PrintPerformancePerSecond(System.nanoTime() - T0, count)+" ops.");
+       
+       T0 = System.nanoTime();
+       for (int i = 0; i < count; ++i)
+         {
+           SimplifyName_Regex1(test);
+         }
+       LOG.info("SimplifyName_Regex1: " + DurationUtil.PrintPerformancePerSecond(System.nanoTime() - T0, count)+" ops.");
+       
+       T0 = System.nanoTime();
+       for (int i = 0; i < count; ++i)
+         {
+           SimplifyName_Regex2(test);
+         }
+       LOG.info("SimplifyName_Regex2: " + DurationUtil.PrintPerformancePerSecond(System.nanoTime() - T0, count)+" ops.");
+       
+       T0 = System.nanoTime();
+       for (int i = 0; i < count; ++i)
+         {
+           SimplifyName_Regex3(test);
+         }
+       LOG.info("SimplifyName_Regex3: " + DurationUtil.PrintPerformancePerSecond(System.nanoTime() - T0, count)+" ops.");
+       
+       T0 = System.nanoTime();
+       for (int i = 0; i < count; ++i)
+         {
+           SimplifyName_Regex4(test);
+         }
+       LOG.info("SimplifyName_Regex4: " + DurationUtil.PrintPerformancePerSecond(System.nanoTime() - T0, count)+" ops.");
+     }
   }
