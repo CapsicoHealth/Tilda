cde8052c8b194120580cde21709de1d8f39208fc
Laurent Hasson <ldhasson@users.noreply.github.com>
2017-06-26 09:41:34 -0400

    -Updated EncryptionUtil.getToken method, added test case. -Added Apache Commons Lang library

--- src/tilda/utils/EncryptionUtil.java
+++ src/tilda/utils/EncryptionUtil.java
@@ -19,6 +19,7 @@ package tilda.utils;
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tomcat.util.codec.binary.Base64;
@@ -45,12 +46,29 @@ public class EncryptionUtil
           }
       }
 
-
+    /**
+     * A short-hand version of {@link #getToken(int, boolean)} with alphaNumOnly set to false
+     * @param size The size of the returned String. If size is < 8, then it will be changed to 8. If it's > 254, it will be changed to 254.
+     * @return A string of random characters of a minimum size of 8, and maximum size of 254.
+     */
     public static String getToken(int size)
       {
-        SecureRandom random = new SecureRandom();
-        byte bytes[] = new byte[size < 16 ? 16 : size];
-        random.nextBytes(bytes);
-        return bytes.toString();
+        return getToken(size, false);
+      }
+    /**
+     * Generates a random string with "size" characters.
+     * @param size The size of the returned String. If size is < 8, then it will be changed to 8. If it's > 254, it will be changed to 254.
+     * @param alphaNumOnly Whether the returned random string should only contain alphanumeric characters or not.
+     * @return A string of random characters of a minimum size of 8, and maximum size of 254.
+     */
+    public static String getToken(int size, boolean alphaNumOnly)
+      {
+        if (size < 8)
+          size = 8;
+        else if (size > 254)
+          size = 254;
+        char[] possibleCharacters = (new String(alphaNumOnly == true ? "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+        : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?")).toCharArray();
+        return RandomStringUtils.random(size, 0, possibleCharacters.length - 1, false, false, possibleCharacters, new SecureRandom());
       }
   }
