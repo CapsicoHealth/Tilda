77211abdedbc182ba4f735b4bf23966e0bdc42c5
Laurent Hasson <ldhasson@users.noreply.github.com>
2019-09-11 01:47:31 -0400

    #425 Unit test data test case

---
+++ ut/tilda/data_test/TestQuestionAnswer_Json.java
@@ -0,0 +1,25 @@
+/*
+ Tilda V1.0 template application class.
+*/
+
+package tilda.data_test;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+This is the application class <B>Data_TestQuestionAnswer</B> mapped to the table <B>TILDATEST.TestQuestionAnswer</B>.
+@see tilda.data_test._Tilda.TILDA__TESTQUESTIONANSWER
+*/
+public class TestQuestionAnswer_Json extends tilda.data_test._Tilda.TILDA__TESTQUESTIONANSWER_Json
+ {
+   protected static final Logger LOG = LogManager.getLogger(TestQuestionAnswer_Json.class.getName());
+
+   protected TestQuestionAnswer_Json() { }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//   Implement your customizations, if any, below.
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ }
