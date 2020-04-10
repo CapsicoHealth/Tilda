6a1369ba1d645d0015f1c624180650b836fe7f68
Laurent Hasson <ldhasson@users.noreply.github.com>
2020-04-09 23:31:27 -0400

    #522 Added more methods to JSON Util to handle arrays

---
+++ src/tilda/utils/json/elements/ElementZonedDateTimeArray.java
@@ -0,0 +1,26 @@
+package tilda.utils.json.elements;
+
+import java.io.Writer;
+import java.time.ZonedDateTime;
+
+import tilda.utils.json.JSONUtil;
+
+public class ElementZonedDateTimeArray implements ElementDef
+  {
+    public ElementZonedDateTimeArray(String Name, ZonedDateTime[] Val)
+      {
+        _Name = Name;
+        _Val = Val;
+      }
+
+    protected final String          _Name;
+    protected final ZonedDateTime[] _Val;
+
+    @Override
+    public void print(Writer Out, boolean FirstElement, String Header)
+    throws Exception
+      {
+        Out.write(Header);
+        JSONUtil.print(Out, _Name, FirstElement, _Val);
+      }
+  }
