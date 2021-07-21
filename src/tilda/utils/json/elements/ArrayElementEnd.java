492e416fb8963b8076f6068928ec94c31fc56ed2
Laurent Hasson <ldhasson@users.noreply.github.com>
2021-07-21 13:19:07 -0400

    #628 FHIR/GCP capabilities
    - JSONPrinter array element support

---
+++ src/tilda/utils/json/elements/ArrayElementEnd.java
@@ -0,0 +1,22 @@
+package tilda.utils.json.elements;
+
+import java.io.Writer;
+
+import tilda.utils.json.JSONUtil;
+
+public class ArrayElementEnd implements ElementDef
+  {
+    public ArrayElementEnd(String Name)
+      {
+        _Name = Name;
+      }
+    protected final String _Name;
+
+    @Override
+    public void print(Writer Out, boolean FirstElement, String Header)
+    throws Exception
+      {
+        JSONUtil.startend(Out, '}');
+        JSONUtil.newline(Out);
+      }
+  }
