53c5d5942110096df2127e9845be3d0f874edf99
Laurent Hasson <410503+ldhasson@users.noreply.github.com>
2025-06-04 02:24:16 -0400

    Migrations not working for clone and history tables #853

--- Tilda/src/tilda/parsing/parts/Migration.java
+++ Tilda/src/tilda/parsing/parts/Migration.java
@@ -22,6 +22,7 @@ import java.util.List;
 import com.google.gson.annotations.SerializedName;
 
 import tilda.parsing.ParserSession;
+import tilda.utils.CollectionUtil;
 
 public class Migration
   {
@@ -33,7 +34,7 @@ public class Migration
 //  @SerializedName("drops"  ) public List<MigrationDrop>   _Drops   = new ArrayList<MigrationDrop  >();
     /*@formatter:on*/
 
-    public transient Schema              _Parent;
+    public transient Schema       _Parent;
 
     public boolean validate(ParserSession PS, Schema Parent)
       {
@@ -44,6 +45,8 @@ public class Migration
           {
             MigrationRename M = _Renames.get(i);
             M.validate(PS, Parent);
+            // We have to check for the cloning feature: if we have to rename a column from the source table, we have to rename
+            // as well the column for all the clones.
             if (M._Object != null && M._Object._CloneAs != null)
               for (Cloner cl : M._Object._CloneAs)
                 {
@@ -51,17 +54,64 @@ public class Migration
                   M2._ObjectName = cl._Name;
                   _Renames.add(M2);
                 }
+            // And let's not forget about History as a special case of clones
+            if (M._Object != null && M._Object._HistoryObj != null)
+              {
+                MigrationRename M2 = new MigrationRename(M);
+                M2._ObjectName = M._Object._HistoryObj._Name;
+                _Renames.add(M2);
+              }
           }
 
         // DROP is currently being removed from feature list as per #58. Too complex with lots of issues.
-//        for (MigrationDrop M : _Drops)
-//          M.validate(PS, Parent);
+        // for (MigrationDrop M : _Drops)
+        // M.validate(PS, Parent);
 
         for (MigrationMove M : _Moves)
-          M.validate(PS, Parent);
+          {
+            if (M._Objects != null)
+              {
+                List<String> L = new ArrayList<String>();
+                for (Object obj : M._Objects)
+                  {
+                    // We have to check for the cloning feature: if we have to move a source table from a schema to this schema,
+                    // we also have to move the clones.
+                    if (obj._CloneAs != null)
+                      for (Cloner cl : obj._CloneAs)
+                        L.add(cl._Name);
+                    // And move the history table
+                    if (obj._HistoryObj != null)
+                      L.add(obj._HistoryObj._Name);
+                  }
+                L = CollectionUtil.append(L, M._ObjectNames);
+                M._ObjectNames = CollectionUtil.toStringArray(L);
+                M.validate(PS, Parent);
+              }
+          }
 
-        for (MigrationNotNull M : _NotNulls)
-          M.validate(PS, Parent);
+        for (int i = 0; i < _NotNulls.size(); ++i)
+          {
+            MigrationNotNull M = _NotNulls.get(i);
+            M.validate(PS, Parent);
+            // We have to check for the cloning feature: if we are adding a new column to a source table,
+            // we also have to add it to the clones.
+            if (M._Object != null && M._Object._CloneAs != null)
+              for (Cloner cl : M._Object._CloneAs)
+                {
+                  MigrationNotNull M2 = new MigrationNotNull(M);
+                  M2._ObjectName = cl._Name;
+                  M2.validate(PS, Parent);
+                  _NotNulls.add(M2);
+                }
+            // And history table too
+            if (M._Object != null && M._Object._HistoryObj != null)
+              {
+                MigrationNotNull M2 = new MigrationNotNull(M);
+                M2._ObjectName = M._Object._HistoryObj._Name;
+                M2.validate(PS, Parent);
+                _NotNulls.add(M2);
+              }
+          }
 
         return Errs == PS.getErrorCount();
       }
@@ -71,7 +121,7 @@ public class Migration
         for (MigrationNotNull mnn : _NotNulls)
           for (Column c : mnn._Columns)
             if (col.getFullName().equals(c.getFullName()) == true)
-             return mnn;
+              return mnn;
         return null;
       }
 
